a8baeee898eac11a31a54fb5e52fe1e7e5ee0c1f
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-03-22 18:16:43 +0700

    update article list view

--- lib/features/article_list/view/article_list_view.dart
+++ lib/features/article_list/view/article_list_view.dart
@@ -5,6 +5,7 @@ import 'package:tech_crunch_news/features/article_list/bloc/article_list_bloc.da
 import 'package:tech_crunch_news/features/article_list/bloc/article_list_event.dart';
 import 'package:tech_crunch_news/features/article_list/bloc/article_list_state.dart';
 import 'package:tech_crunch_news/repositories/api/api_service.dart';
+import 'package:tech_crunch_news/repositories/models/article.dart';
 import 'package:tech_crunch_news/repositories/news_repository/news_repository.dart';
 
 class ArticleListPage extends StatelessWidget {
@@ -39,41 +40,113 @@ class ArticleListView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('TechCrunch News'),
       ),
-      body: BlocBuilder<ArticleListBloc, ArticleListState>(
-        builder: (context, state) {
-          if (state is ArticleListLoadErrorState) {
-            return Center(
-              child: Text(state.message),
-            );
-          } else if (state is ArticleListLoadSuccessState) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final article = state.articles[index];
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Find the Latest Update',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 24),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Search for News',
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Featured Stories',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+              child: BlocBuilder<ArticleListBloc, ArticleListState>(
+                builder: (context, state) {
+                  if (state is ArticleListLoadErrorState) {
+                    return Center(
+                      child: Text(state.message),
+                    );
+                  } else if (state is ArticleListLoadSuccessState) {
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        final article = state.articles[index];
 
-                return ListTile(
-                  leading: Image.network(
-                    article.urlToImage,
-                    fit: BoxFit.cover,
-                    width: 100,
-                  ),
-                  title: Text(
-                    article.title,
-                    maxLines: 1,
-                  ),
-                  subtitle: Text(
-                    article.description,
-                    maxLines: 2,
-                  ),
-                );
-              },
-              itemCount: state.articles.length,
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                        return NewsCard(article: article);
+                      },
+                      itemCount: state.articles.length,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            article.urlToImage,
+            width: 100,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  article.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Read news',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 4),
+                Container(
+                  height: 2,
+                  width: 48,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
