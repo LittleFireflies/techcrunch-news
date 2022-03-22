bbb87ed8d8513c698f954bcb74f890e02ee916ca
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-03-23 02:22:25 +0700

    fix theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
 import 'package:tech_crunch_news/features/article_detail/view/article_detail_view.dart';
 import 'package:tech_crunch_news/features/article_list/view/article_list_view.dart';
@@ -8,7 +7,7 @@ import 'package:tech_crunch_news/features/article_webview/view/article_webview.d
 import 'package:tech_crunch_news/repositories/api/api_service.dart';
 import 'package:tech_crunch_news/repositories/models/article.dart';
 import 'package:tech_crunch_news/repositories/news_repository/news_repository.dart';
-import 'package:tech_crunch_news/theme/color_scheme.dart';
+import 'package:tech_crunch_news/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,10 +26,7 @@ class MyApp extends StatelessWidget {
       ),
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          textTheme: GoogleFonts.robotoTextTheme(),
-          colorScheme: NewsColorScheme.lightColorScheme,
-        ),
+        theme: NewsTheme.mainTheme(),
         initialRoute: ArticleListPage.routeName,
         onGenerateRoute: (RouteSettings settings) {
           switch (settings.name) {
---
+++ lib/theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tech_crunch_news/theme/color_scheme.dart';
+
+class NewsTheme {
+  static ThemeData mainTheme() {
+    return applyAllStyles(
+      ThemeData.from(colorScheme: NewsColorScheme.lightColorScheme),
+    );
+  }
+
+  static ThemeData applyAllStyles(
+    ThemeData themeData,
+  ) {
+    return themeData.copyWith(
+      textTheme: GoogleFonts.robotoTextTheme(),
+    );
+  }
+}
